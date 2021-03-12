a256f851469fd3e0787f014238ca6e3248fb97d9
hasnularief <arief.hasnul@gmail.com>
2021-03-12 15:37:25 +0700

    livewire page create

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,10 +2,61 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Pages extends Component
 {
+    public $modalFormVisible = false;
+    public $slug;
+    public $title;
+    public $content;
+
+    public function updatedTitle($value)
+    {
+        $this->slug = Str::slug($value, '-');
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required'
+        ];
+    }
+
+    public function create() 
+    {
+        $this->validate();
+        Page::create($this->modelData());
+        $this->modalFormVisible = false;
+        $this->resetVars();
+    }
+
+    public function modelData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content
+        ];
+    }
+
+    public function resetVars()
+    {
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+    }
+
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+
     public function render()
     {
         return view('livewire.pages');
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,3 +1,50 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="p-6">
+    <x-jet-button wire:click="createShowModal">
+        {{ __('Create') }}
+    </x-jet-button>
+
+    <x-jet-dialog-modal wire:model="modalFormVisible">
+        <x-slot name="title">
+            {{ __('Save Page') }}
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ __('Title') }}" />
+                <x-jet-input id="title" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="title" />
+                @error('title')<span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="slug" value="{{ __('Slug') }}" />
+                <x-jet-input id="slug" class="block mt-1 w-full" type="text" wire:model="slug" />
+                @error('slug')<span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="title" value={{ __('Content') }} />
+                <div class="rounded-md shadow-sm">
+                    <div class="mt-1 bg-white">
+                        <div class="body-content" wire:ignore>
+                            <trix-editor 
+                                class="trix-content" 
+                                x-ref="trix" 
+                                wire:model.debounce.100000ms="content" 
+                                wire:key="trix-content-unique-key"
+                            ></trix-editor>
+                        </div>
+                    </div>
+                </div>
+                @error('content')<span class="error">{{ $message }}</span> @enderror
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+
+            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                {{ __('Save') }}
+            </x-jet-button>
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
